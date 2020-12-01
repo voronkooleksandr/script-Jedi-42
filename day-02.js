8fda21e7a4d2ee2c662b624c7004f3d915eb40c9
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-01 14:02:19 +0200

    Update day-02.js

--- day-02.js
+++ day-02.js
@@ -1,29 +1,33 @@
+// Зробив стрілочними функціями на github (без return, в 1 рядок та з присвоєнням const через Function Expression)
+
 //https://www.codewars.com/kata/571edd157e8954bab500032d/train/javascript
 
 var v1 = 50;
 (v2 = 100), (v3 = 150), (v4 = 200), (v5 = 2), (v6 = 250);
-function equal1() {
-  var a = v1;
-  var b = v1;
-  return a + b;
-}
-function equal2() {
-  var a = v6;
-  var b = v3;
-  return a - b;
-}
-function equal3() {
-  var a = v1;
-  var b = v5;
-  return a * b;
-}
-function equal4() {
-  var a = v4;
-  var b = v5;
-  return a / b;
-}
-function equal5() {
-  var a = v6;
-  var b = v3;
-  return a % b;
-}
+const equal1 = () => v1 + v1;
+const equal2 = () => v6 - v3;
+const equal3 = () => v1 * v5;
+const equal4 = () => v4 / v5;
+const equal5 = () => v6 % v3;
+
+https://www.codewars.com/kata/571edea4b625edcb51000d8e/train/javascript
+
+var a1 = "A",
+  a2 = "a",
+  b1 = "B",
+  b2 = "b",
+  c1 = "C",
+  c2 = "c",
+  d1 = "D",
+  d2 = "d",
+  e1 = "E",
+  e2 = "e",
+  n1 = "N",
+  n2 = "n";
+const Dad = () => d1 + a2 + d2;
+const Bee = () => b1 + e2 + e2;
+const banana = () => b2 + a2 + n2 + a2 + n2 + a2;
+const answer1 = () => "no";
+const answer2 = () => "no";
+const answer3 = () => "yes";
+

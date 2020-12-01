33be64accf5ee26e2c066c9eba7ba3c23f1fd16f
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-01 17:54:44 +0200

    Update day-02.js

--- day-02.js
+++ day-02.js
@@ -41,3 +41,24 @@ function animal(obj) {
   };
   return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
 }
+
+//https://www.codewars.com/kata/571effabb625ed9b0600107a/train/javascript
+
+function getLength(arr) {
+  return arr.length;
+}
+function getFirst(arr) {
+  return arr[0];
+}
+function getLast(arr) {
+  return arr[arr.length - 1];
+}
+function pushElement(arr) {
+  var el = 1;
+  arr.push(el);
+  return arr;
+}
+function popElement(arr) {
+  arr.pop();
+  return arr;
+}

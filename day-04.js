5086e95e94ba42f5536d0fbaed6560f3de18d0f1
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-03 16:50:27 +0200

    Update day-04.js

--- day-04.js
+++ day-04.js
@@ -16,3 +16,18 @@ function padIt(Oleksandr, n) {
 
   return result.join("");
 }
+
+// 10. https://www.codewars.com/kata/5721a78c283129e416000999/train/javascript
+
+function pickIt(arr) {
+  let odd = [],
+    even = [];
+  for (let i of arr) {
+    if (i % 2 === 0) {
+      even.push(i);
+    } else {
+      odd.push(i);
+    }
+  }
+  return [odd, even];
+}

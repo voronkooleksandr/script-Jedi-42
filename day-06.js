1ca026ac3e0c02799ce28f3273dee02d38a53914
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-05 17:35:13 +0200

    Create day-06.js

---
+++ day-06.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+
+function unusualFive() {
+  const fiveAlways = ["a", "r", "r", "a", "y"];
+  return fiveAlways.length;
+}
+

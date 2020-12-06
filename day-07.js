8d90df6022a4c98a5ce44d10a1fa6dbf4de8c580
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-06 13:07:34 +0200

    Update day-07.js

--- day-07.js
+++ day-07.js
@@ -1,2 +1,18 @@
 // https://www.codewars.com/kata/55e7650c8d894146be000095/train/javascript
+// головне в роботі розробника - уважність!..тут з підказками, тому 100 не чесно буде :)
+
+function validateMessage(msg) {
+  if (msg === null) throw new ReferenceError("Message is null!");
+  if (typeof msg !== "string")
+    throw new TypeError(
+      `Message should be of type string but was of type ${typeof msg}!`
+    );
+  if (msg.length === 0 || msg.length > 255)
+    throw new RangeError(`Message contains ${msg.length} characters!`);
+  if (msg.includes("<") && msg.includes(">")) {
+    return false;
+  } else {
+    return true;
+  }
+}
 

14b5fe731703a402bb39ff53dc7fb207cd6a1748
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-07 20:05:17 +0200

    Update day-08.js

--- day-08.js
+++ day-08.js
@@ -9,4 +9,19 @@ function howManySmaller(arr, n) {
   return array.length;
 }
 
-//
+// https://www.codewars.com/kata/5722fd3ab7162a3a4500031f/train/javascript
+
+function whatNumberIsIt(n) {
+  {
+    if (n > Number.MAX_VALUE) return "Input number is Number.POSITIVE_INFINITY";
+    if (n === Number.MAX_VALUE) return "Input number is Number.MAX_VALUE";
+    if (n === Number.MIN_VALUE) return "Input number is Number.MIN_VALUE";
+    if (n < -Number.MAX_VALUE)
+      return "Input number is Number.NEGATIVE_INFINITY";
+    if (isNaN(n)) return "Input number is Number.NaN";
+  }
+  if (n === Number(n)) {
+    return "Input number is " + n;
+  }
+  return "Input number is Number(n)";
+}

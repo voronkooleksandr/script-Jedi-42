34ae707a2e88820fe8eedc7ac324bedc4f7c3bb2
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-09 23:12:54 +0200

    Update day-09.js

--- day-09.js
+++ day-09.js
@@ -2,3 +2,20 @@
 
 const firstToLast = (str, c) =>
   str.indexOf(c) === -1 ? -1 : str.lastIndexOf(c) - str.indexOf(c);
+
+// http://www.codewars.com/kata/57274562c8dcebe77e001012
+// 3 годині читав про reduce метод, який би мав замінити цикл за допомогою колбек ф-ції але --  по відео копі-пастив але розумію всі рядки
+
+function cutIt(arr) {
+  let minLen = arr[0].length;
+  for (i = 1; i < arr.length; i++) {
+    if (arr[i].length < minLen) {
+      minLen = arr[i].length;
+    }
+  }
+  const cutArray = [];
+  for (i = 0; i < arr.length; i++) {
+    cutArray.push(arr[i].slice(0, minLen));
+  }
+  return cutArray;
+}

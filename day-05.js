29c724227610b1d4fa07c3ac3118e0195f8e3aed
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-09 10:24:49 +0200

    Update day-05.js

--- day-05.js
+++ day-05.js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
 
 function bonusTime(salary, bonus) {
-  const money = 0;
+  let money = 0;
   if (bonus === true) {
     money = salary * 10;
   } else {

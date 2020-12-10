ce70eb682ab45b5e719854b75de879ee6923fda7
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-10 13:35:25 +0200

    Create day-10.js

---
+++ day-10.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/5728203b7fc662a4c4000ef3/train/javascript
+
+// https://www.codewars.com/kata/57284d23e81185ae6200162a/train/javascript
+
+// https://www.codewars.com/kata/5729b103dd8bac11a900119e/train/javascript

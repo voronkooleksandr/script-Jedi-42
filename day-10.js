26aee888ff1f760a82e3cd4dcda9a83ef76c09f4
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-10 23:27:47 +0200

    Update day-10.js

--- day-10.js
+++ day-10.js
@@ -1,5 +1,28 @@
 // https://www.codewars.com/kata/5728203b7fc662a4c4000ef3/train/javascript
 
+function alienLanguage(str) {
+  const myString = str.split(" ");
+  const result = [];
+  for (i = 0; i < myString.length; i++) {
+    const myStringMain = myString[i]
+      .slice(0, myString[i].length - 1)
+      .toUpperCase();
+    const endLetter = myString[i].slice(-1).toLowerCase();
+    const myStringNew = myStringMain + endLetter;
+    result.push(myStringNew);
+  }
+  return result.join(" ");
+}
+
 // https://www.codewars.com/kata/57284d23e81185ae6200162a/train/javascript
 
 // https://www.codewars.com/kata/5729b103dd8bac11a900119e/train/javascript
+// працює) але сам вигляд кода мені не дуже через prettier
+
+function fiveLine(s) {
+  const d = s.trim();
+  const result = `${d}\n${d.repeat(2)}\n${d.repeat(3)}\n${d.repeat(
+    4
+  )}\n${d.repeat(5)}`;
+  return result;
+}

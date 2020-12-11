f625e828b0656d250402b743bc2115a75a73f8aa
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-11 22:07:48 +0200

    Update day-12.js

--- day-12.js
+++ day-12.js
@@ -1,2 +1,18 @@
 // https://www.codewars.com/kata/playing-with-sets-equal-or-not/train/javascript
 
+function areEqual(s1, s2) {
+  if (s1.size === s2.size) {
+    for (const item of s1) {
+      if (!s2.has(item)) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function notEqual(s1, s2) {
+  return !areEqual(s1, s2);
+}

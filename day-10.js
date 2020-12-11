8cf83497a093484f587669b3c091540655ef81a3
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-11 13:55:34 +0200

    Update day-10.js

--- day-10.js
+++ day-10.js
@@ -16,6 +16,22 @@ function alienLanguage(str) {
 
 // https://www.codewars.com/kata/57284d23e81185ae6200162a/train/javascript
 
+function topSecret(str) {
+  let chars = str.split("");
+  for (var i = 0; i < chars.length; i++) {
+    if (chars[i].charCodeAt() >= 100 && chars[i].charCodeAt() <= 123) {
+      chars[i] = String.fromCharCode(chars[i].charCodeAt() - 3);
+    } else if (chars[i].charCodeAt() >= 97 && chars[i].charCodeAt() <= 99) {
+      chars[i] = String.fromCharCode(chars[i].charCodeAt() + 23);
+    } else if (chars[i].charCodeAt() >= 68 && chars[i].charCodeAt() <= 90) {
+      chars[i] = String.fromCharCode(chars[i].charCodeAt() - 3);
+    } else if (chars[i].charCodeAt() >= 65 && chars[i].charCodeAt() <= 67) {
+      chars[i] = String.fromCharCode(chars[i].charCodeAt() + 23);
+    }
+  }
+  return chars.join("");
+}
+
 // https://www.codewars.com/kata/5729b103dd8bac11a900119e/train/javascript
 // працює) але сам вигляд кода мені не дуже через prettier
 

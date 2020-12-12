32b65e5024540453d4fe86ea1026bd439875a829
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-12 12:52:06 +0200

    Update day-11.js

--- day-11.js
+++ day-11.js
@@ -37,3 +37,33 @@ describe("`Set` lets you store unique values of any type", function () {
 // 2. set.add()
 // https://tddbin.com/#?
 
+describe("`add()` appends a new element to the end of a Set object.", function () {
+  let set;
+  beforeEach(() => (set = new Set()));
+  it("adds every value, of any type, only once", function () {
+    const fn = () => {};
+    set.add(1);
+    set.add(1);
+    set.add(fn);
+    set.add(fn);
+    assert.equal(set.size, 2);
+  });
+  it("is chainable", function () {
+    set.add(1);
+    set.add(2);
+    assert.equal(set.has(2), true);
+  });
+  it("call without params adds undefined", function () {
+    set.add();
+    assert.equal(set.has(void 0), true);
+  });
+  it("0, -0 and +0 are equal", function () {
+    set.add(0);
+    set.add(-0);
+    set.add(+0);
+    assert.equal(set.has(+0), true);
+  });
+});
+
+// 3. set.delete()
+// https://tddbin.com/#?

7ce8544ce872827aa72cf8d30d6f8e83f10186db
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-12 16:05:46 +0200

    Update day-11.js

--- day-11.js
+++ day-11.js
@@ -186,3 +186,30 @@ describe("`Set` API overview", function () {
 });
 
 // set.clear() https://tddbin.com/#?
+
+describe("`clear()` removes all elements from a Set object.", function () {
+  let set;
+  beforeEach(() => (set = new Set()));
+  it("`set.size` becomes 0", function () {
+    set.add("one").add(2);
+    set.clear();
+    let actualSize = set.size;
+    assert.equal(actualSize, set.size);
+  });
+  it("the iterator `set.entries()` will not contain any items", function () {
+    set.add("one").add(2);
+    set.clear();
+    const { done } = set.entries().next();
+    assert.equal(done, true);
+  });
+  it("any call to `set.has()` returns false", function () {
+    set.add("one").add(2);
+    set.delete(2);
+    set.has();
+    assert.deepEqual(set.has(2), false);
+  });
+  it("returns `undefined`", function () {
+    let actualReturn;
+    assert.equal(actualReturn, set.clear());
+  });
+});

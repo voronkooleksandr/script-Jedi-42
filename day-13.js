4e1c703494864749cbe375ecac35d6c6e20dbeab
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-12 21:57:38 +0200

    Update day-13.js

--- day-13.js
+++ day-13.js
@@ -1,2 +1,42 @@
 // basics http://tddbin.com/#?kata=es6/language/map/basics
 
+describe("`Map` is a key/value map", function () {
+  it("`Map` is a new global constructor function", function () {
+    const typeOfMap = "function";
+    assert.equal(typeOfMap, typeof Map);
+  });
+  it("provides `new Map().set()` to add key+value pair, `get()` to read it by key", function () {
+    let map = new Map();
+    map.set("key", "value");
+    const value = map.get("key");
+    assert.equal(value, "value");
+  });
+  it("`has()` tells if map has the given key", function () {
+    let map = new Map();
+    map.set("key", "value");
+    const hasIt = map.has("key");
+    assert.equal(hasIt, true);
+  });
+  it("a map is iterable", function () {
+    let map = new Map();
+    map.set("1", "one");
+    map.set("2", "two");
+    const mapAsArray = Array.from(map); // hint: kata #29 http://tddbin.com/#?kata=es6/language/array-api/from
+    assert.deepEqual(mapAsArray, [
+      ["1", "one"],
+      ["2", "two"],
+    ]);
+  });
+  it("complex types can be keys", function () {
+    const obj = { x: 1 };
+    const otherObj = { x: 1 };
+    let map = new Map();
+    map.set(obj, "");
+    map.set(otherObj, "");
+    map.delete(otherObj);
+    assert.equal(map.has(otherObj), false);
+  });
+});
+
+// map.get() http://tddbin.com/#?kata=es6/language/map/get
+

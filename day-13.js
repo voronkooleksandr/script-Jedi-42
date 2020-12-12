a6417e5f150b9bd06ac4486b256ae3e8f3f5f153
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-12 23:42:33 +0200

    Update day-13.js

--- day-13.js
+++ day-13.js
@@ -153,5 +153,36 @@ function assertMapsEqual(map1, map2) {
 
 // map.has() http://tddbin.com/#?kata=es6/language/map/has
 
-
+describe("`map.has()` indicates whether an element with a key exists", function () {
+  it("finds nothing in an empty map", function () {
+    let map = new Map();
+    const hasKey = map.has();
+    assert.equal(hasKey, false);
+  });
+  it("finds an element by it`s key", function () {
+    let map = new Map([["key", "VALUE"]]);
+    const hasKey = map.has("key");
+    assert.equal(hasKey, true);
+  });
+  it("finds `undefined` as key too", function () {
+    let map = new Map([[void 0, "not defined key"]]);
+    const hasUndefinedAsKey = map.has();
+    assert.equal(hasUndefinedAsKey, true);
+  });
+  it("does not coerce keys", function () {
+    let map = new Map([["1", "one"]]);
+    const findsStringOne = true;
+    assert.equal(map.has("1"), findsStringOne);
+  });
+  it("after removal (using `map.delete(<key>)`) it doesnt find the element anymore", function () {
+    let map = new Map([[1, "one"]]);
+    map.delete(1);
+    assert.equal(map.has(1), false);
+  });
+  it("adding an item (using `map.set(key, value)`) later will make `has()` return true", function () {
+    let map = new Map();
+    map.set();
+    assert.equal(map.has(void 0), true);
+  });
+});
 

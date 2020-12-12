265e9c686ad01f9d90df1eebcdf52223c393f109
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-12 15:12:22 +0200

    Update day-11.js

--- day-11.js
+++ day-11.js
@@ -112,4 +112,77 @@ describe("`set.delete()` deletes an element from a set", function () {
 
 // Set API http://tddbin.com/#?kata=es6/language/set/api
 
+describe("`Set` API overview", function () {
+  const api = [
+    "size",
+    "add",
+    "clear",
+    "delete",
+    "entries",
+    "forEach",
+    "has",
+    "keys",
+    "values",
+  ];
+  let set;
+  beforeEach(function () {
+    set = new Set(api);
+  });
+  it("a Set can be created from an array", function () {
+    let set = new Set([]);
+    const api = Array.from(set);
+    assert.deepEqual(Array.from(set), api);
+  });
+  it("`size` is the number of values", function () {
+    const theSize = set.count;
+    const api = set;
+    assert.equal(theSize, api.length);
+  });
+  it("`add()` appends the given value", function () {
+    // Hint: To make the content of `api` and `set` consistent you can add the
+    // `Symbol.iterator` to the `set`. Strongly speaking it is missing in the API.
+    set.add(Symbol.iterator);
+    assert.equal(set.size, api.length + 1);
+  });
+  it("`clear()` removes all elements", function () {
+    set.clear();
+    assert.equal(set.size, 0);
+  });
+  it("`delete()` removes the given value", function () {
+    set.delete("size");
+    assert.equal(set.size, api.length - 1);
+  });
+  it("`entries()` returns an iterator for all values", function () {
+    const expectedEntries = api.map((entry) => [entry, entry]);
+    const actualEntries = set.entries();
+    assert.deepEqual([...actualEntries], expectedEntries);
+  });
+  it("`forEach()` calls a callback for each value", function () {
+    let values = [];
+    api.forEach((value) => {
+      values.push(value);
+    });
+    assert.deepEqual(values, api);
+  });
+  it("`has()` returns true if the given value is in the set", function () {
+    const propertyName = "has";
+    assert.equal(set.has(propertyName), true);
+  });
+  describe("returns an iterator that contains all values", function () {
+    // In order to be alike to `Map`, `keys()` and `values()` are essentially the same thing for a `Set`.
+    it("`keys()`", function () {
+      const allKeys = set.keys();
+      assert.deepEqual([...allKeys], api);
+    });
+    it("`values()`", function () {
+      const allValues = set.values();
+      assert.deepEqual([...allValues], api);
+    });
+    it("`[Symbol.iterator]()`", function () {
+      const iteratorKey = Symbol.iterator;
+      assert.deepEqual([...set[iteratorKey]()], api);
+    });
+  });
+});
 
+// set.clear() https://tddbin.com/#?

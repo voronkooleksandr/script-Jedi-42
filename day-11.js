2fedb1e0428ecf509c0f577dbfdc9838254fda3c
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-12 12:39:12 +0200

    Update day-11.js

--- day-11.js
+++ day-11.js
@@ -1,7 +1,39 @@
-//
+// 1. Set Basics
+// https://tddbin.com/#?
 
-//
+describe("`Set` lets you store unique values of any type", function () {
+  it("`Set` is a new global constructor function", function () {
+    const typeOfSet = "function";
+    assert.equal(typeOfSet, typeof Set);
+  });
+  it("every value in a set is unique", function () {
+    let set = new Set();
+    set.add(1);
+    set.add(2);
+    const actualSize = 2;
+    assert.equal(actualSize, set.size);
+  });
+  it('the string "1" is different to the number 1', function () {
+    let set = new Set();
+    set.add(1);
+    set.add("1");
+    assert.equal(set.size, 2);
+  });
+  it("even NaN is equal to NaN", function () {
+    let set = new Set();
+    set.add(NaN);
+    set.add(NaN);
+    assert.equal(set.size, 1);
+  });
+  it("+0 and -0 are seen as equal", function () {
+    let set = new Set();
+    set.add(+0);
+    set.add(0);
+    set.add(-0);
+    assert.deepEqual([...set.values()], [+0]);
+  });
+});
 
-//
+// 2. set.add()
+// https://tddbin.com/#?
 
-//

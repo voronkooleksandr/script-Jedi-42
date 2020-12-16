0316b1f0e1f8fbdea991a185f5a9b7509a6b96cc
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-16 21:36:37 +0200

    Update day-16.js

--- day-16.js
+++ day-16.js
@@ -1,4 +1,4 @@
-// https://www.codewars.com/kata/572df796914b5ba27c000c90/train/javascript
+// #25: methods of arrayObject---reverse() and sort() http://www.codewars.com/kata/572df796914b5ba27c000c90
 
 function sortIt(arr) {
   return arr
@@ -10,5 +10,19 @@ function sortIt(arr) {
     );
 }
 
-// https://www.codewars.com/kata/572fdeb4380bb703fc00002c/train/javascript
+// #26: methods of arrayObject---map() http://www.codewars.com/kata/572fdeb4380bb703fc00002c
 
+function isolateIt(arr) {
+  return arr.slice().map((elementofArray) => {
+    const len = elementofArray.length;
+    elementofArray = elementofArray.split("");
+    len % 2 === 0
+      ? elementofArray.splice(len / 2, 0, "|")
+      : elementofArray.splice(len / 2, 1, "|");
+    return elementofArray.join("");
+  });
+}
+
+// #27: methods of arrayObject---filter() http://www.codewars.com/kata/573023c81add650b84000429
+
+// #28: methods of arrayObject---every() and some() http://www.codewars.com/kata/57308546bd9f0987c2000d07

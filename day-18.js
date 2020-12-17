e1e30e976a80bf78243d5235bb67ff5a95188105
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-17 21:20:21 +0200

    Update day-18.js

--- day-18.js
+++ day-18.js
@@ -34,4 +34,23 @@ describe("`Array.from` converts an array-like object or list into an Array", () 
 
 // Array.of() http://tddbin.com/#?kata=es6/language/array-api/of
 
+describe("`Array.of` creates an array with the given arguments as elements", () => {
+  it("dont mix it up with `Array(10)`, where the argument is the array length", () => {
+    const arr = Array.of(10);
+    assert.deepEqual(arr, [10]);
+  });
+  it("puts all arguments into array elements", () => {
+    const arr = Array.of(1, 2);
+    assert.deepEqual(arr, [1, 2]);
+  });
+  it("takes any kind and number of arguments", () => {
+    const starter = [1, 2];
+    const end = [3, "4"];
+    const arr = Array.of(starter, ...end);
+    assert.deepEqual(arr, [[1, 2], 3, "4"]);
+  });
+});
+
+// [].fill() http://tddbin.com/#?kata=es6/language/array-api/fill
+
 

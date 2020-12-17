6a32fcb4646d1cd33b79b65fb9437e60dfad64ce
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-17 21:37:40 +0200

    Update day-18.js

--- day-18.js
+++ day-18.js
@@ -53,4 +53,29 @@ describe("`Array.of` creates an array with the given arguments as elements", () 
 
 // [].fill() http://tddbin.com/#?kata=es6/language/array-api/fill
 
+describe("`Array.prototype.fill` can fill up an array with one value", () => {
+  it("`fill(0)` will populate `0` into each array element", function () {
+    const arr = new Array(3).fill(0);
+    assert.deepEqual(arr, [0, 0, 0]);
+  });
+  it("fill only changes content, adds no new elements", function () {
+    const arr = new Array(1).fill(0);
+    assert.deepEqual(arr, [0]);
+  });
+  it("second parameter to `fill()` is the position where to start filling", function () {
+    const fillPosition = 2;
+    const arr = [1, 2, 3].fill(42, fillPosition);
+    assert.deepEqual(arr, [1, 2, 42]);
+  });
+  it("third parameter is the position where filling stops", function () {
+    const fillStartAt = 1;
+    const fillEndAt = 2;
+    const arr = [1, 2, 3].fill(42, fillStartAt, fillEndAt);
+    assert.deepEqual(arr, [1, 42, 3]);
+  });
+});
+
+
+// [].find() http://tddbin.com/#?kata=es6/language/array-api/find
+
 

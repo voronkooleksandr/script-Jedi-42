95a37a1ed26154235830da66cb1977db639e4483
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-19 22:38:10 +0200

    Update day-15.js

--- day-15.js
+++ day-15.js
@@ -9,7 +9,22 @@ const shuffleIt = (arrayNumbers, ...numericArrays) => {
 
 //#23: methods of arrayObject---push(), pop(), shift() and unshift() http://www.codewars.com/kata/572af273a3af3836660014a1
 
-
+function infiniteLoop(arr, d, n) {
+  if (d === "left") {
+    for (let i = 0; i < n; i++) {
+      arr[2].push(arr[0].shift());
+      arr[1].push(arr[2].shift());
+      arr[0].push(arr[1].shift());
+    }
+  } else {
+    for (let i = 0; i < n; i++) {
+      arr[2].unshift(arr[1].pop());
+      arr[1].unshift(arr[0].pop());
+      arr[0].unshift(arr[2].pop());
+    }
+  }
+  return arr;
+}
 
 //#24: methods of arrayObject---splice() and slice() http://www.codewars.com/kata/572cb264362806af46000793
 

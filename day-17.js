4f1341d55d8eca4de172d82af2b207c573c1bc1e
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-21 20:14:26 +0200

    Update day-17.js

--- day-17.js
+++ day-17.js
@@ -7,3 +7,20 @@ function bigToSmall(arr) {
 
 // #30: methods of arrayObject---reduce() and reduceRight() http://www.codewars.com/kata/573156709a231dcec9000ee8
 
+function tailAndHead(arr) {
+  let head = [];
+  for (i = 0; i < arr.length - 1; i++) {
+    head.push(Number(String(arr[i]).slice(-1)));
+  }
+  let tail = [];
+  for (i = 1; i < arr.length; i++) {
+    tail.push(Number(String(arr[i]).slice(0, 1)));
+  }
+  const sumHeadandTail = [];
+  for (i = 0; i < head.length; i++) {
+    sumHeadandTail.push(head[i] + tail[i]);
+  }
+  return sumHeadandTail.reduce((a, b) => a * b);
+}
+
+//

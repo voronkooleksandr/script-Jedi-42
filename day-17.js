00ee8185869c99f03c533244fc80621550ccb69a
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-22 23:00:26 +0200

    Update day-17.js

--- day-17.js
+++ day-17.js
@@ -31,3 +31,6 @@ function blackAndWhite(arr) {
   if (!arr.includes(5) || !arr.includes(13)) return "It's a white array";
 }
 
+// Implement own reduce https://www.codewars.com/kata/array-number-reduce/train/javascript
+
+//I understand the task +- 80% metod of reduce() from the video but don"t be coppy/past. :). Wait this task in the next weeks!

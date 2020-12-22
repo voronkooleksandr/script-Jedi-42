232a918a5ad4600129a43b86435aa72b5be9634d
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-22 15:18:29 +0200

    Update day-17.js

--- day-17.js
+++ day-17.js
@@ -23,4 +23,11 @@ function tailAndHead(arr) {
   return sumHeadandTail.reduce((a, b) => a * b);
 }
 
-//
+// #31: methods of arrayObject---isArray() indexOf() and toString() http://www.codewars.com/kata/5732b0351eb838d03300101d
+
+function blackAndWhite(arr) {
+  if (!Array.isArray(arr)) return "It's a fake array";
+  if (arr.includes(5) && arr.includes(13)) return "It's a black array";
+  if (!arr.includes(5) || !arr.includes(13)) return "It's a white array";
+}
+

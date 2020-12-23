b78b0908835188773a80f841c82092703ffe2893
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-23 16:46:43 +0200

    Update day-23.js

--- day-23.js
+++ day-23.js
@@ -1,1 +1,5 @@
+// Unpacking arguments http://www.codewars.com/kata/unpacking-arguments
 
+const spread = (lambda, args) => lambda(...args);
+
+// For the sake of argument http://www.codewars.com/kata/for-the-sake-of-argument

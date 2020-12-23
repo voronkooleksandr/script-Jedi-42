e9943579a03427f88440a6ff4eb5b7a82bc88b0b
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-23 21:17:47 +0200

    Update day-23.js

--- day-23.js
+++ day-23.js
@@ -3,3 +3,13 @@
 const spread = (lambda, args) => lambda(...args);
 
 // For the sake of argument http://www.codewars.com/kata/for-the-sake-of-argument
+
+function numbers(...param) {
+  const paramArray = Array.from(param);
+  const result = paramArray.every((i) => {
+    if (Number(i) === i || isNaN(i)) {
+      return true;
+    }
+  });
+  return result;
+}

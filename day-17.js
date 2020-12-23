7c3b26bddc64ada8ef9804a5f7fcd5a5eac165b6
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-23 22:51:05 +0200

    Update day-17.js

--- day-17.js
+++ day-17.js
@@ -1,8 +1,10 @@
 // #29: methods of arrayObject---concat() and join() http://www.codewars.com/kata/5731861d05d14d6f50000626
 
 function bigToSmall(arr) {
-  const result = [].concat(...arr);
-  return (sortDescending = result.sort((a, b) => b - a).join(">"));
+  let result = [];
+  for (let i = 0; i < arr.length; i++)
+    result = result.concat(arr[i]).sort((a, b) => b - a);
+  return result.join(">");
 }
 
 // #30: methods of arrayObject---reduce() and reduceRight() http://www.codewars.com/kata/573156709a231dcec9000ee8

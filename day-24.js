1f929ee521a798ca305c1b7a2ce61646d33a25c8
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-24 15:03:20 +0200

    Update day-24.js

--- day-24.js
+++ day-24.js
@@ -1,8 +1,8 @@
 // Duplicate arguments http://www.codewars.com/kata/duplicate-arguments
 
-function solution(...arguments) {
-  const arrayOfObject = [...arguments].length;
-  const setOfObject = [...new Set(arguments)].length;
+function solution(...arg) {
+  const arrayOfObject = [...arg].length;
+  const setOfObject = [...new Set(arg)].length;
   return setOfObject != arrayOfObject;
 }
 

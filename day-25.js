83ab002934559a4c9f774df9eacf93a351cda0d3
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-25 14:39:45 +0200

    Create day-25.js

---
+++ day-25.js
@@ -0,0 +1,7 @@
+// Call function using apply http://www.codewars.com/kata/write-javascripts-call-function-using-apply
+
+Function.prototype.call = function (arg1, ...arg2) {
+  return this.apply(arg1, arg2);
+};
+
+// Anonymous returns http://www.codewars.com/kata/anonymous-returns

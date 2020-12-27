512f7eea439fc2d33f34c097d2769631fdd1bd49
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-27 21:59:48 +0200

    Update day-26.js

--- day-26.js
+++ day-26.js
@@ -1,5 +1,12 @@
 // Power of bind http://www.codewars.com/kata/power-bind
 
+Function.prototype.bind = function (ctx) {
+  const func = this;
+  return function (...args) {
+    return func.apply(this === global ? ctx : this);
+  };
+};
+
 // Sort with Arrow Functions https://www.codewars.com/kata/sort-with-arrow-functions
 
 const OrderPeople = function (people) {

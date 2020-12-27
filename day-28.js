4d6acf46fd8cabc8677423f9bd5583b4a26e9564
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-27 22:13:47 +0200

    Update day-28.js

--- day-28.js
+++ day-28.js
@@ -1,3 +1,9 @@
 // Function within a function http://www.codewars.com/kata/a-function-within-a-function
 
+function always(n) {
+  return function () {
+    return n;
+  };
+}
+
 // Singleton pattern https://www.codewars.com/kata/singleton-pattern/train/javascript

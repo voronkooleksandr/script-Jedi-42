61f0ffbeb10d264dfd25062601b6749c5a2e7c4f
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-28 15:41:33 +0200

    Update day-27.js

--- day-27.js
+++ day-27.js
@@ -1,6 +1,10 @@
 // Understanding closures - the  basics https://www.codewars.com/kata/56b71b1dbd06e6d88100092a
 
-
+function buildFun(n) {
+  let res = [];
+  for (let i = 0; i < n; i++) res.push(() => i);
+  return res;
+}
 
 // Shifty closures https://www.codewars.com/kata/shifty-closures/train/javascript
 

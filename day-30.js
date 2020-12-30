e6e345430f9189e7f7ba887d40e2969024dfff9d
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-30 18:28:09 +0200

    Update day-30.js

--- day-30.js
+++ day-30.js
@@ -13,3 +13,20 @@ function isSantaClausable(santa) {
 
 // Cylon Evolution http://www.codewars.com/kata/cylon-evolution
 
+function Cylon(model) {
+  this.model = model;
+}
+Cylon.prototype.attack = function () {
+  return "Destroy all humans!";
+};
+
+function HumanSkin(model) {
+  this.model = model;
+}
+HumanSkin.prototype = Cylon.prototype;
+HumanSkin.prototype.infiltrate = function () {
+  return "Infiltrate the colonies";
+};
+
+// Simple Web Framework #1: Create a basic router https://www.codewars.com/kata/588a00ad70720f2cd9000005/train/javascript
+

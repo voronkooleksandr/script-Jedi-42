ddb1bb597052eaea7baf652cc1961a83e58f86b1
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-30 18:30:49 +0200

    Create day-31.js

---
+++ day-31.js
@@ -0,0 +1,23 @@
+// Object Literal
+// Basics http://tddbin.com/#?kata=es6/language/object-literal/basics
+
+// Computed Properties http://tddbin.com/#?kata=es6/language/object-literal/computed-properties
+
+// Getter http://tddbin.com/#?kata=es6/language/object-literal/getter
+
+// Setter http://tddbin.com/#?kata=es6/language/object-literal/setter
+
+// Class
+// Creation http://tddbin.com/#?kata=es6/language/class/creation
+
+// Accessors http://tddbin.com/#?kata=es6/language/class/accessors
+
+// Static http://tddbin.com/#?kata=es6/language/class/static
+
+// Extends http://tddbin.com/#?kata=es6/language/class/extends
+
+// More Extends http://tddbin.com/#?kata=es6/language/class/more-extends
+
+// Super in Method http://tddbin.com/#?kata=es6/language/class/super-in-method
+
+// Super in Constructor http://tddbin.com/#?kata=es6/language/class/super-in-constructor

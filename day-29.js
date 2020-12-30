b62f2ae738e0b13c32067a7ed1cdb1d8df28f209
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-30 14:30:36 +0200

    Update day-29.js

--- day-29.js
+++ day-29.js
@@ -1,3 +1,7 @@
 // How new works https://www.codewars.com/kata/how-new-works
 
+const myObj = {};
+myObj.__proto__ = MyObject.prototype;
+myObj.constructor = MyObject.call(myObj);
+
 // Replicate new http://www.codewars.com/kata/replicate-new

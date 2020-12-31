28a75e90b9c797e9cbe60c5d92326760f0fc384c
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-31 15:55:43 +0200

    Update day-31.js

--- day-31.js
+++ day-31.js
@@ -165,6 +165,56 @@ describe("An object literal can also contain setters", () => {
 // Class
 // Creation http://tddbin.com/#?kata=es6/language/class/creation
 
+describe("Class creation", () => {
+  it("is as simple as `class XXX {}`", function () {
+    class TestClass {}
+    const instance = new TestClass();
+    assert.equal(typeof instance, "object");
+  });
+  it("a class is block scoped", () => {
+    {
+      class Inside {}
+    }
+    assert.equal(typeof Inside, "undefined");
+  });
+  it("the `constructor` is a special method", function () {
+    class User {
+      constructor(id) {
+        this.id = id;
+      }
+    }
+    const user = new User(42);
+    assert.equal(user.id, 42);
+  });
+  it("defining a method by writing it inside the class body", function () {
+    class User {
+      writesTests() {
+        return false;
+      }
+    }
+    const notATester = new User();
+    assert.equal(notATester.writesTests(), false);
+  });
+  it("multiple methods need no commas (opposed to object notation)", function () {
+    class User {
+      wroteATest() {
+        this.everWroteATest = true;
+      }
+      isLazy() {
+        return !this.everWroteATest;
+      }
+    }
+    const tester = new User();
+    assert.equal(tester.isLazy(), true);
+    tester.wroteATest();
+    assert.equal(tester.isLazy(), false);
+  });
+  it("anonymous class", () => {
+    const classType = typeof class {};
+    assert.equal(classType, "function");
+  });
+});
+
 // Accessors http://tddbin.com/#?kata=es6/language/class/accessors
 
 // Static http://tddbin.com/#?kata=es6/language/class/static

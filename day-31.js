248c0626b023a970cb61b4ff76ff5d38dde918d4
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2020-12-31 22:22:23 +0200

    Update day-31.js

--- day-31.js
+++ day-31.js
@@ -410,3 +410,48 @@ describe('Inside a class use `super` to access parent methods', () => {
 });
 
 // Super in Constructor http://tddbin.com/#?kata=es6/language/class/super-in-constructor
+
+describe('Inside a class`s constructor `super()` can be used', () => {
+  it('`extend` a class and use `super()` to call the parent constructor', () => {
+    class A {constructor() { this.levels = 0; }}
+    class B extends A {
+      constructor() {
+        super();
+        this.levels++;
+        this.levels++;
+      }
+    }
+    assert.equal(new B().levels, 2);
+  });
+  it('`super()` may also take params', () => {
+    class A {constructor(startValue=1, addTo=1) { this.counter = startValue + addTo; }}
+    class B extends A {
+      constructor(...args) { 
+        super(...args);
+        this.counter++;
+      }
+    }
+    assert.equal(new B(42, 2).counter, 45);
+  });
+  it('it is important where you place your `super()` call!', () => {
+    class A {inc() { this.countUp = 1; }}
+    class B extends A {
+      inc() {
+        this.countUp = 2;
+        super.inc();
+        return this.countUp;
+      }
+    }
+    assert.equal(new B().inc(), 1);
+  });
+  it('use `super.constructor` to find out if there is a parent constructor', () => {
+    class ParentClassA {constructor() {"parent"}}
+    class B extends ParentClassA {
+      constructor() {
+        super();
+        this.isTop = '' + super.constructor;
+      }
+    }
+    assert(new B().isTop.includes('ParentClassA'), new B().isTop);
+  });
+});

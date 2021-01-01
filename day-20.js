8801e02b158329ef8ba79011d944fc00579668c1
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-01 22:04:15 +0200

    Update day-20.js

--- day-20.js
+++ day-20.js
@@ -1,5 +1,16 @@
 // #37: Unlock new weapon---RegExp Object http://www.codewars.com/kata/5735e39313c205fe39001173
 
+function countAnimals(animals, count) {
+  const result = [];
+  for (let i = 0; i < count.length; i++) {
+    result.push((animals.match(new RegExp(count[i], "g")) || []).length);
+    console.log(result);
+  }
+  return result;
+}
+
 // #38: Regular Expression--""^"",""$"", ""."" and test() http://www.codewars.com/kata/573975d3ac3eec695b0013e0
+
+
 
 // #39: Regular Expression--""?"", ""*"", ""+"" and ""{}"" https://www.codewars.com/kata/573bca07dffc1aa693000139

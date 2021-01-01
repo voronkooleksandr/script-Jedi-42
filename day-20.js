f8791f551e35a5d8d57c08ccac6b175bbfc6cfa7
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-01 19:26:02 +0200

    Create day-20.js

---
+++ day-20.js
@@ -0,0 +1,5 @@
+// #37: Unlock new weapon---RegExp Object http://www.codewars.com/kata/5735e39313c205fe39001173
+
+// #38: Regular Expression--""^"",""$"", ""."" and test() http://www.codewars.com/kata/573975d3ac3eec695b0013e0
+
+// #39: Regular Expression--""?"", ""*"", ""+"" and ""{}"" https://www.codewars.com/kata/573bca07dffc1aa693000139

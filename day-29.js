a8f54cc723e0fed72000509e3949b50d9cddbe33
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-01 17:15:26 +0200

    Update day-29.js

--- day-29.js
+++ day-29.js
@@ -5,3 +5,12 @@ myObj.__proto__ = MyObject.prototype;
 myObj.constructor = MyObject.call(myObj);
 
 // Replicate new http://www.codewars.com/kata/replicate-new
+
+function nouveau(Constructor, ...arguments) {
+  const objectNew = Object.create(Constructor.prototype);
+  const result = Constructor.apply(objectNew, arguments);
+  return result && (typeof result === "object" || typeof result === "function")
+    ? result
+    : objectNew;
+}
+

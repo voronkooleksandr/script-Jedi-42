af91281540910b3ccbdd1b2aebf3b4172dcbcefe
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-01 23:23:31 +0200

    Update day-20.js

--- day-20.js
+++ day-20.js
@@ -11,6 +11,16 @@ function countAnimals(animals, count) {
 
 // #38: Regular Expression--""^"",""$"", ""."" and test() http://www.codewars.com/kata/573975d3ac3eec695b0013e0
 
-
+function findSimilarity(string, word) {
+  const changeOfString =
+    word[0] + word.slice(1, -1).replace(/.+/g, ".+") + word.slice(-1);
+  const regularOfString = new RegExp("^" + changeOfString + "$");
+  console.log(regularOfString);
+  console.log(changeOfString);
+  return string
+    .split(" ")
+    .filter((item) => item.length === word.length && regularOfString.test(item))
+    .join(" ");
+}
 
 // #39: Regular Expression--""?"", ""*"", ""+"" and ""{}"" https://www.codewars.com/kata/573bca07dffc1aa693000139

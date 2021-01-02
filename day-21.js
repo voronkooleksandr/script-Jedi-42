10643bef7f3dedc4792f6b9583f27147cfeaebfe
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-02 13:09:31 +0200

    Create day-21.js

---
+++ day-21.js
@@ -0,0 +1,7 @@
+// #40: Regular Expression--""|"", ""[]"" and ""()"" http://www.codewars.com/kata/573d11c48b97c0ad970002d4
+
+const regex = /(http:\/\/|https:\/\/)[a-z0-9.]+\.(com|net)/gi;
+
+// #41: Regular Expression--"""" http://www.codewars.com/kata/573e6831e3201f6a9b000971
+
+// #42: Regular Expression--(?:), (?=) and (?!) http://www.codewars.com/kata/573fb9223f9793e485000453

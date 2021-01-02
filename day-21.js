0e9e0a4d7d7f460e1b617733236da46cd897b5b2
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-02 16:37:33 +0200

    Update day-21.js

--- day-21.js
+++ day-21.js
@@ -4,4 +4,6 @@ const regex = /(http:\/\/|https:\/\/)[a-z0-9.]+\.(com|net)/gi;
 
 // #41: Regular Expression--"""" http://www.codewars.com/kata/573e6831e3201f6a9b000971
 
+const regex = /\b(\w)(\w){0,1}(\w){0,1}\w{0,1}\3\2\1\b/g;
+
 // #42: Regular Expression--(?:), (?=) and (?!) http://www.codewars.com/kata/573fb9223f9793e485000453

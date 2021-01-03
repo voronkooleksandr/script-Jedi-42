90bc9566e4e273269a69d2ebe0eff570399e57ed
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-03 15:46:30 +0200

    Create day-33.js

---
+++ day-33.js
@@ -0,0 +1,10 @@
+//TDD Bin Object.is()
+//Object.is() http://tddbin.com/#?kata=es6/language/object-api/is
+//TDD Bin Reflect http://tddbin.com/#?kata=es6/language/reflect/basics
+
+//Basics
+//Reflect.apply() http://tddbin.com/#?kata=es6/language/reflect/apply
+//Reflect.getPrototypeOf() http://tddbin.com/#?kata=es6/language/reflect/getprototypeof
+//Reflect.construct() http://tddbin.com/#?kata=es6/language/reflect/construct
+//Reflect.defineProperty() http://tddbin.com/#?kata=es6/language/reflect/defineproperty
+//TDD Bin Modules import https://tddbin.com/?664#?kata=es6/language/modules/import

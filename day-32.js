e7c10b531c3415a95bcaf534569207c000506f31
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-03 18:04:17 +0200

    Update day-32.js

--- day-32.js
+++ day-32.js
@@ -1,9 +1,14 @@
 // "this"" is a problem - http://www.codewars.com/kata/this-is-a-problem
 
-function NameMe(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.name = this.firstName + " " + this.lastName;
+function NameMe(first, last) {
+  this.firstName = first;
+  this.lastName = last;
 }
+
+NameMe.prototype = {
+  get name() {
+    return this.firstName + " " + this.lastName;
+  },
+};
 
 // ""this"" is an other problem - https://www.codewars.com/kata/this-is-an-other-problem

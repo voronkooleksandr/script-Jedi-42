4c8d9de737f00228944d6857b4780afab09ccecf
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-03 12:58:11 +0200

    Create day-32.js

---
+++ day-32.js
@@ -0,0 +1,9 @@
+// "this"" is a problem - http://www.codewars.com/kata/this-is-a-problem
+
+function NameMe(firstName, lastName, name) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.name = this.firstName + " " + this.lastName;
+}
+
+// ""this"" is an other problem - https://www.codewars.com/kata/this-is-an-other-problem

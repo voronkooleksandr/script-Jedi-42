dda9afaa7cb7c5bea51a662199c6355fe21c5ea6
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-04 22:45:01 +0200

    Create day-34.js

---
+++ day-34.js
@@ -0,0 +1,7 @@
+//Adam and Eve - https://www.codewars.com/kata/basic-subclasses-adam-and-eve/
+
+
+//Labrador - https://www.codewars.com/kata/56ff9b53140fcca90b000530/
+
+
+//Classy Classes - https://www.codewars.com/kata/55a144eff5124e546400005a

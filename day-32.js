d3e4c9f1dcb4073514b1e5a9d8f5bc8c0d16bc8d
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-06 21:35:50 +0200

    Update day-32.js

--- day-32.js
+++ day-32.js
@@ -12,3 +12,21 @@ NameMe.prototype = {
 };
 
 // ""this"" is an other problem - https://www.codewars.com/kata/this-is-an-other-problem
+
+function NamedOne(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
+
+NamedOne.prototype = {
+  get fullName() {
+    return this.firstName + " " + this.lastName;
+  },
+  set fullName(change) {
+    let newName = change.split(" ");
+    if (newName.length == 2) {
+      this.firstName = newName[0];
+      this.lastName = newName[1];
+    }
+  },
+};

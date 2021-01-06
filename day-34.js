5ffb3c07be5c2a00478771705ac8bdbb05e94190
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-06 23:33:08 +0200

    Update day-34.js

--- day-34.js
+++ day-34.js
@@ -14,20 +14,7 @@ class Woman extends Human {};
 
 //Labrador - https://www.codewars.com/kata/56ff9b53140fcca90b000530/
 
-class Dogs {
-  constructor(name, age, gender, species, size, master, loyal) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
-    this.species = species;
-    this.legs = 4;
-    this.size = size;
-    this.master = master;
-    this.loyal = loyal;
-  }
-}
-
-class Labrador extends Dogs {
+class Labrador extends Dog {
   constructor(name, age, gender, master) {
     super(name, age, gender, "Labrador", "Large", master, true);
   }

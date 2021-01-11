5ea725536d6206eb511fdd5eafb8c16883d45e7b
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-11 22:28:38 +0200

    Update day-38.js

--- day-38.js
+++ day-38.js
@@ -34,6 +34,45 @@ describe("The native array is a built-in iterable object", function () {
 
 //string http://tddbin.com/#?kata=es6/language/iterator/string
 
+describe("The native string is a built-in iterable object", function () {
+  const s = "abc";
+
+  describe("string is iterable", function () {
+    it("the string`s object key `Symbol.iterator` is a function", function () {
+      const isA = typeof s[Symbol.iterator];
+      assert.equal(isA, "function");
+    });
+    it("use `Array.from()` to make an array out of any iterable", function () {
+      const arr = Array.from(s);
+      assert.deepEqual(arr, ["a", "b", "c"]);
+    });
+  });
+
+  describe("a string`s iterator", function () {
+    let iterator;
+    beforeEach(function () {
+      iterator = s[Symbol.iterator]();
+    });
+    it("has a special string representation", function () {
+      const description = iterator.toString();
+      assert.equal(description, "[object String Iterator]");
+    });
+    it("`iterator.next()` returns an object according to the iterator protocol", function () {
+      const value = iterator.next();
+      assert.deepEqual(value, { done: false, value: "a" });
+    });
+    it("the after-last call to `iterator.next()` says done=true, no more elements", function () {
+      const arrEmpty = [1, 2, 3];
+      const iterator = arrEmpty[Symbol.iterator]();
+      iterator.next();
+      iterator.next();
+      iterator.next();
+      assert.equal(iterator.next().done, true);
+    });
+  });
+});
 
 //protocol http://tddbin.com/#?kata=es6/language/iterator/protocol
+
+
 //usage http://tddbin.com/#?kata=es6/language/iterator/usages

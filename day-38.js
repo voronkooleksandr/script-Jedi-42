bbcd7f981c4a8dfc994b41591ee8b8602e19f1d9
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-11 19:40:49 +0200

    Create day-38.js

---
+++ day-38.js
@@ -0,0 +1,6 @@
+//array http://tddbin.com/#?kata=es6/language/iterator/array
+
+
+//string http://tddbin.com/#?kata=es6/language/iterator/string
+//protocol http://tddbin.com/#?kata=es6/language/iterator/protocol
+//usage http://tddbin.com/#?kata=es6/language/iterator/usages

891f8c310f80399f1e04840d517a3d2330c04248
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-11 19:39:02 +0200

    Update day-36.js

--- day-36.js
+++ day-36.js
@@ -329,7 +329,72 @@ describe("chaining multiple promises can enhance readability", () => {
   });
 });
 
-the API http://tddbin.com/#?kata=es6/language/promise/api
+//the API http://tddbin.com/#?kata=es6/language/promise/api
 
+describe("`Promise` API overview", function () {
+  it("`new Promise()` requires a function as param", () => {
+    const param = () => {};
+    assert.doesNotThrow(() => {
+      new Promise(param);
+    });
+  });
+  describe("resolving a promise", () => {
+    // reminder: the test passes when a fulfilled promise is returned
+    it("via constructor parameter `new Promise((resolve) => { resolve(); })`", () => {
+      const param = () => {
+        resolve();
+      };
+      return new Promise((resolve) => {
+        resolve();
+      });
+    });
+    it("using `Promise.resolve()`", () => {
+      return Promise.resolve("all fine");
+    });
+  });
+  describe("a rejected promise", () => {
+    it("using the constructor parameter", (done) => {
+      const promise = new Promise((reject) => {
+        reject();
+      });
+      Promise.reject()
+        .then(() => done(new Error("The promise is expected to be rejected.")))
+        .catch(() => done());
+    });
+    it("via `Promise.reject()`", (done) => {
+      const promise = Promise.reject();
+      promise
+        .then(() => done(new Error("The promise is expected to be rejected.")))
+        .catch(() => done());
+    });
+  });
+  describe("`Promise.all()`", () => {
+    it("`Promise.all([p1, p2])` resolves when all promises resolve", () => {
+      return Promise.all([
+        Promise.resolve(),
+        Promise.resolve(),
+        Promise.resolve(),
+      ]);
+    });
+    it("`Promise.all([p1, p2])` rejects when a promise is rejected", (done) => {
+      Promise.all([Promise.reject()])
+        .then(() => done(new Error("The promise is expected to be rejected.")))
+        .catch(() => done());
+    });
+  });
+  describe("`Promise.race()`", () => {
+    it("`Promise.race([p1, p2])` resolves/reject when one of the promises resolves/rejects", () => {
+      return Promise.race([Promise.resolve(), Promise.reject()]);
+    });
+    it("`Promise.race([p1, p2])` rejects when one of the promises rejects", (done) => {
+      Promise.race([Promise.reject()])
+        .then(() => done(new Error("The promise is expected to be rejected.")))
+        .catch(() => done());
+    });
+    it("`Promise.race([p1, p2])` order matters (and timing)", () => {
+      return Promise.race([Promise.resolve(), Promise.reject()]);
+    });
+  });
+});
 
-promise.catch() http://tddbin.com/#?kata=es6/language/promise/catch
+//promise.catch() http://tddbin.com/#?kata=es6/language/promise/catch

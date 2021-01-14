aebfe93ef9d426f4e13f9da301e0a255d66abdd8
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-14 17:29:59 +0200

    Update day-40.js

--- day-40.js
+++ day-40.js
@@ -38,8 +38,40 @@ describe('Generators can be created in multiple ways', function() {
 
 //iterator http://tddbin.com/#?kata=es6/language/generator/iterator
 
+describe('Generators returns an iterable object', function() {
+  function* generatorFunction(){
+    yield 1;
+    yield 2;
+  }
+  let generator;
+  beforeEach(() => {
+    generator = generatorFunction();
+  });
+  it('a generator returns an object', function() {
+    const typeOfTheGenerator = 'object';
+    assert.equal(typeOfTheGenerator, typeof generator);
+  });
+  it('a generator object has a key `Symbol.iterator`', function() {
+    const key = Symbol.iterator;
+    assert.equal(key in generator, true);
+  });
+  it('the `Symbol.iterator` is a function', function() {
+    const theType = typeof generator[Symbol.iterator];
+    assert.equal(theType, 'function');
+  });
+  it('can be looped with `for-of`, which expects an iterable', function() {
+    function iterateForOf(){
+      for (let value of generator) {
+        // no statements needed
+      }
+    }
+    assert.doesNotThrow(iterateForOf);
+  });
+});
 
 //yield expressions http://tddbin.com/#?kata=es6/language/generator/yield
+
+
 //send value to a generator http://tddbin.com/#?kata=es6/language/generator/send-value
 //send function to a generator http://tddbin.com/#?kata=es6/language/generator/send-function
 //`return` inside a generator function http://tddbin.com/#?kata=es6/language/generator/return

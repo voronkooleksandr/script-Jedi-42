9b04a2e419b18ad1371008b5fb0bc98e01ab8a03
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-14 13:08:07 +0200

    Create day-40.js

---
+++ day-40.js
@@ -0,0 +1,8 @@
+//creation http://tddbin.com/#?kata=es6/language/generator/creation
+
+
+//iterator http://tddbin.com/#?kata=es6/language/generator/iterator
+//yield expressions http://tddbin.com/#?kata=es6/language/generator/yield
+//send value to a generator http://tddbin.com/#?kata=es6/language/generator/send-value
+//send function to a generator http://tddbin.com/#?kata=es6/language/generator/send-function
+//`return` inside a generator function http://tddbin.com/#?kata=es6/language/generator/return

47067a1ac5a93461354350692ee4d145fdda9fe8
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-14 13:05:34 +0200

    Update day-39.js

--- day-39.js
+++ day-39.js
@@ -1,1 +1,22 @@
 // Create iterator	https://www.codewars.com/kata/5c743cec901022438549964a
+
+const createIterator = (array) => {
+  const iterator = {
+    index: 0,
+    next() {
+      if (iterator.index >= array.length) {
+        return {
+          value: undefined,
+          done: true,
+        };
+      } else {
+        return {
+          value: array[iterator.index++],
+          done: false,
+        };
+      }
+    },
+  };
+  return iterator;
+};
+

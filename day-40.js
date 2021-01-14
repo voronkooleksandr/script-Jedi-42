ae29a14b09e6d59a5205cbbf7063801770829230
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-14 22:39:17 +0200

    Update day-40.js

--- day-40.js
+++ day-40.js
@@ -71,7 +71,57 @@ describe('Generators returns an iterable object', function() {
 
 //yield expressions http://tddbin.com/#?kata=es6/language/generator/yield
 
+describe('Generator - `yield` is used to pause and resume a generator function', () => {
+  function* generatorFunction() {
+    yield 'hello';
+    yield 'world';
+  }
+  let generator;
+
+  beforeEach(function() {
+    generator = generatorFunction();
+  });
+  it('converting a generator to an array (using `Array.from`) resumes the generator until all values are received', () => {
+    let values = Array.from(generatorFunction());
+    assert.deepEqual(values, ['hello', 'world']);
+  });
+  describe('after the first `generator.next()` call', function() {
+    it('the value is "hello"', function() {
+      const {value} = generator.next();
+      assert.equal(value, 'hello');
+    });
+    it('and `done` is false', function() {
+      const {done} = generator.next();
+      assert.equal(done, false);
+    });
+  });
+  describe('after the second `next()` call', function() {
+    let secondItem;
+    beforeEach(function() {
+      
+      secondItem = generator.next();
+    });
+    it('`value` is "world"', function() {
+      let {value} = generator.next();
+      assert.equal(value, 'world');
+    });
+    it('and `done` is still false', function() {
+      const {done} = secondItem;
+      assert.equal(done, false);
+    });
+  });
+  describe('after stepping past the last element, calling `next()` that often', function() {
+    it('`done` property equals true, since there is nothing more to iterator over', function() {
+      generator.next();
+      generator.next();
+      let {done} = generator.next();
+      assert.equal(done, true);
+    });
+  });
+});
 
 //send value to a generator http://tddbin.com/#?kata=es6/language/generator/send-value
+
+
 //send function to a generator http://tddbin.com/#?kata=es6/language/generator/send-function
 //`return` inside a generator function http://tddbin.com/#?kata=es6/language/generator/return

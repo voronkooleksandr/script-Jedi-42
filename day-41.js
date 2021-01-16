777d6e950fbb0e55a52c578f363dbadad4b349ff
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-16 11:21:39 +0200

    Update day-41.js

--- day-41.js
+++ day-41.js
@@ -1,1 +1,14 @@
-//
+// Generators #1 https://www.codewars.com/kata/basics-generators-number-1/javascript
+
+function* generator() {
+  const end = Infinity;
+  let item;
+  for (let i = 1; i < end; i++) {
+    if (item) {
+      i = item;
+    }
+    item = yield i;
+  }
+}
+
+// Generators #2 https://www.codewars.com/kata/5637ead70013386e30000027/

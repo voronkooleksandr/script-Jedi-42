567eadd24de316bfd352c836304c2d647e519c09
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-17 19:28:47 +0200

    Update day-37.js

--- day-37.js
+++ day-37.js
@@ -1,1 +1,27 @@
 // Promises Made me Broken	https://www.codewars.com/kata/587593285448632b8d000143
+
+function submitOrder(user) {
+  let shoppingCart, zipCode, shippingRate, orderSuccessful;
+  // Get the current user's shopping cart
+  OrderAPI.getShoppingCartAsync(user)
+    .then(function (cart) {
+      shoppingCart = cart;
+      return CustomerAPI.getProfileAsync(user);
+    })
+    // Also look up the ZIP code from their profile
+    .then(function (profile) {
+      zipCode = profile.zipCode;
+    })
+    // Calculate the shipping fees
+    .then(function (delivery) {
+      shippingRate = calculateShipping(shoppingCart, zipCode);
+      return OrderAPI.placeOrderAsync(shoppingCart, shippingRate);
+    })
+    // Submit the order
+    .then(function (success) {
+      orderSuccessful = success;
+    });
+  console.log(
+    `Your order ${orderSuccessful ? "was" : "was NOT"} placed successfully`
+  );
+}

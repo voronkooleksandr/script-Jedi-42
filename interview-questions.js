4376305e6cd4521dacce811097e5deb9a4cb4457
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-18 14:47:27 +0200

    Update interview-questions.js

--- interview-questions.js
+++ interview-questions.js
@@ -1,1 +1,8 @@
 // https://www.codewars.com/collections/interview-questions-8
+
+// 1. https://www.codewars.com/kata/555bfd6f9f9f52680f0000c5/train/javascript
+
+function reverseNumber(n) {
+  return parseInt(n.toString().split("").reverse().join("")) * Math.sign(n);
+}
+

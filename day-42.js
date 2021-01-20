0dc58e88c87530d7aa41370ec781b67098cb20b5
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-01-20 14:32:31 +0200

    Update day-42.js

--- day-42.js
+++ day-42.js
@@ -1,1 +1,11 @@
 // Two Sum	https://leetcode.com/problems/two-sum/
+
+const twoSum = function(nums, target) {
+  for (let i = 0; i <= nums.length; i++) {
+    for (let k = 1; k <= nums.length; k++) {
+      if (target - nums[k] === nums[i]) {
+        return [k, i];
+      }
+    }
+  }
+}

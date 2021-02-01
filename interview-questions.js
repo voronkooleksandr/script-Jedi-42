a55163035608a38dec4608e47276fc51be00c098
Voronko Oleksandr <61209463+voronkooleksandr@users.noreply.github.com>
2021-02-01 17:00:09 +0200

    Update interview-questions.js

--- interview-questions.js
+++ interview-questions.js
@@ -1,12 +1,14 @@
 // https://www.codewars.com/collections/interview-questions-8
 
 // 1. https://www.codewars.com/kata/555bfd6f9f9f52680f0000c5/train/javascript
+// Reverse a Number
 
 function reverseNumber(n) {
   return parseInt(n.toString().split("").reverse().join("")) * Math.sign(n);
 }
 
 // 2. https://www.codewars.com/kata/52c31f8e6605bcc646000082/train/javascript
+// Two Sum
 
 function twoSum(numbers, target) {
   for (let i = 0; i <= numbers.length; i++) {
